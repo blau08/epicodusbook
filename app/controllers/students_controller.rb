9deeb8f1a6165681d5efd96e70a70fe8f5d2d71e
Brian <student@epicodus.com>
2015-10-22 12:16:07 -0700

    test

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -44,6 +44,6 @@ class StudentsController < ApplicationController
 
 private
   def student_params
-    params.require(:student).permit(:name, :quote, :url, :image)
+    params.require(:student).permit(:name, :quote, :image)
   end
 end
