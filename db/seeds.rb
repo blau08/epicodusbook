a6c4fe87adadd7d5fe83da4bfc737a7166a7ea4d
Brian <student@epicodus.com>
2015-11-09 10:57:17 -0800

    Got the seed syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Student.create([{ name: 'Brian Lau', quote: "I sayeth, Fly!", url: 'http://i.imgur.com/da8c5eO.jpg'}])
-Student.create([{ name: 'Michael Summers', quote: "General Tso's and Unreal Tournament", url: 'http://i.imgur.com/99eiM8Y.jpg'}])
-Student.create([{ name: 'Kyle Mellander', quote: "My wife is going be my step-mother", url: 'http://i.imgur.com/jT5SPSu.jpg'}])
-Student.create([{ name: 'Andrew Dang', quote: "Hahahha, Joke's On You", url: 'http://i.imgur.com/jJeXtL9.jpg'}])
-Student.create([{ name: 'Aileen Cacayorin', quote: "Spam Aorus & Nintendo", url: 'http://i.imgur.com/jrgismB.jpg'}])
-Student.create([{ name: 'Janice Laset-Parkerson', quote: "We need better child care development centers", url: 'http://i.imgur.com/lJbSXaV.jpg'}])
-Student.create([{ name: 'Brandon Seroyer', quote: "You just gotta put it out there", url: 'http://i.imgur.com/2u295Fd.jpg'}])
+Student.create([{ name: 'Brian Lau', quote: "I sayeth, Fly!", image: File.open(File.join(Rails.root, 'public', 'system', 'categories', 'images', '000', '000', '004', 'medium', '2015_Epicodus_Class_Picture_Funny.jpg'))}])
