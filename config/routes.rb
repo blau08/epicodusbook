5c234d5b03ced16794a91f4dc2029a8a1edc19e5
Brian <blau08@gmail.com>
2015-11-11 13:13:12 -0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users do
+    delete 'logout' => 'sessions#destroy', :as => :destroy_user_session
+  end
   root 'students#index'
   resources :students do
     resources :comments
